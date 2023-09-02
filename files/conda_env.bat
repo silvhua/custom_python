7cf4eb75a3698d1f575f6a9c82baa3189b8f9289
silvhua <silvhua@gmail.com>
2023-09-02 00:50:32 -0700

    conda install -y ipykernel

--- files/conda_env.bat
+++ files/conda_env.bat
@@ -3,4 +3,6 @@ conda install -y openai && ^
 conda install -y sqlalchemy && ^
 pip install psycopg2 && ^
 conda install -c conda-forge -y sshtunnel && ^
-conda install -y requests
+conda install -y requests && ^
+conda install -y ipykernel && ^
+conda install -y seaborn
---
+++ files/conda_env_projects.bat
@@ -0,0 +1,14 @@
+conda create -n projects && ^
+conda activate projects && ^
+conda install -y pandas && ^
+conda install -y openai && ^
+conda install -y flask && ^
+pip install langchain[llms] && ^
+pip install streamlit && ^
+conda install -c anaconda scikit-learn && ^
+conda install -y requests && ^
+conda install -y matplotlib && ^
+conda install -y plotly && ^
+conda install -y seaborn && ^
+conda install -y nbconvert && ^
+conda install -y ipykernel
