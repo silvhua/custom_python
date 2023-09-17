74c3187f9c055641eec789ef9bed7a54a1bdfeb0
silvhua <silvhua@gmail.com>
2023-09-16 17:02:39 -0700

    `print_dict_values_for_key`: `zero_indexed` param

--- wrangling.py
+++ wrangling.py
@@ -480,9 +480,14 @@ def text_to_list(string):
     string = string.strip().replace('\n', "', '")
     return string
 
-def print_dict_values_for_key(results_dict, key):
+def print_dict_values_for_key(results_dict, key, zero_indexed=False):
     """
     Prints the values of a dictionary for a given key.
+
+    Parameters:
+    - results_dict (dict): Dictionary to print values for.
+    - key (str): Dictionary key to print values for.
+    - zero_indexed (bool): If True, prints the index of each value starting from 0. If False, prints the index starting from 1.
     """
     for index, item in enumerate(results_dict):
-        print(f"{index}: {results_dict[item].get(key, 'None').strip()}", end='\n\n')
+        print(f"{index+1 if zero_indexed==False else index}: {results_dict[item].get(key, 'None').strip()}", end='\n\n')
