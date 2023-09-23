fe78e86ab1f29db1d09296095cbccca5295f3e74
silvhua <silvhua@gmail.com>
2023-09-23 07:33:36 -0700

    create datajam conda environment

---
+++ files/conda_env_datajam.bat
@@ -0,0 +1,7 @@
+conda create -n datajam && ^
+conda activate datajam && ^
+conda install -y pandas && ^
+conda install -y openai && ^
+conda install -y requests && ^
+conda install -y ipykernel && ^
+pip install langchain[llms]
