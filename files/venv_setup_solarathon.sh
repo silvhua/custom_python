f9e4e8f1a1e518532ce6498b388b9cd80a18d0e3
silvhua <silvhua@gmail.com>
2023-11-21 11:51:06 -0800

    pip install ipywidgets

--- files/venv_setup_solarathon.sh
+++ files/venv_setup_solarathon.sh
@@ -4,3 +4,4 @@ source solarathon/bin/activate
 pip install farm-haystack -f https://download.pytorch.org/whl/torch_stable.html
 pip install ipykernel
 ipython kernel install --user --name=solarathon
+pip install ipywidgets
