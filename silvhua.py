c9536ff58478e1b3b81e27be0d5010cd884eaf5e
silvhua <silvhua@gmail.com>
2023-11-21 23:32:59 -0800

    created `append_timestamp` function

--- silvhua.py
+++ silvhua.py
@@ -5,6 +5,13 @@ import pickle
 from datetime import datetime
 import json
 
+def append_timestamp(string):
+    """
+    Append timestamp to filename string.
+    """
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H%M")
+    return f'{string}_{timestamp}' 
+
 # 2022-10-27 17:02 Update the sampling function to avoid loading entire dataframe.
 def load_csv(filename,filepath,column1_as_index=False,truncate=None, usecols=None, sep=','):
     """
