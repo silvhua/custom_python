0dbc7cc4600befbde98d7517c53eec1ad1109bfc
silvhua <silvhua@gmail.com>
2023-12-31 14:20:55 -0800

    `plot_int_hist`: update to specify `histfunc` param

--- silvhua_plot.py
+++ silvhua_plot.py
@@ -11,65 +11,82 @@ except:
     print('Matplotlib and Seaborn not installed.')
 
 # Function to plot multiple histograms using Plotly. Show different colours based on classification.
-def plot_int_hist(df, columns=None, color=None, label=1):
+def plot_int_hist(
+    df, groupby, columns=None, classification=None, label=1, 
+    agg='sum',
+    barmode='stack', n_columns=1, height=150, y_order=None
+    ):
     """
     Use Plotly to plot multiple histograms using the specified columns of a dataframe.
+    https://plotly.com/python/histograms/#specify-aggregation-function
+
     Arguments:
     - df: Dataframe.
     - columns (optional): Columns of dataframe on which to create the histogram. If blank, all numeric data will be plotted.
-    - color (optional): Provide name of colum containing binary classification values 0 and 1. 
+    - classification (optional): Provide name of column containing binary classification values (e.g. 0 and 1). 
         Data points classified as 1 will be in red.
-    - label (optional): Label of classification column. Default is 1.
-    
-    Make sure to do the following imports:
-    import plotly.graph_objects as go
-    from plotly.subplots import make_subplots
-    """
-    # import plotly.express as px
-    import plotly.graph_objects as go
-    import plotly 
-    from plotly.subplots import make_subplots
+    - label (optional): Value of classification column. Default is 1.
+    - agg ('count', 'sum', 'avg', 'median', 'mode', 'rms', 'stddev', 'min', 'max', 'first', and 'last'; 
+        string, optional): Aggregate function to apply. Default is sum. 
+    - barmode ('stack', 'group', or 'overlay'; optional): How the different will be shown. Default is 'stack'.
+    - n_columns (optional): Number of columns in the figure. Default is 2.
+    - height (optional): Height of each subplot in pixels. Default is 150.
+    - y_order (optional): List of values to specify the order of the y axis. Default is None.
 
+    """
     if columns == None:
         columns = df.dtypes[df.dtypes != 'object'].index.tolist()
-    fig = make_subplots(rows=round((len(columns)+.5)/2), cols=2,subplot_titles=columns)
+    n_rows = (len(columns)-1) // n_columns + 1
+    fig = make_subplots(rows=n_rows, cols=n_columns, subplot_titles=columns)
     for i, feature in enumerate(columns):
-        if color:
-            bins = dict(
-                start = min(df[feature]),
-                end =  max(df[feature]),
-                # size=
-            )
-            zero = df[df[color] != label]
-            one = df[df[color] == label]
-            fig.add_trace(go.Histogram(x=zero[feature],
+        if classification:
+            zero = df.sort_values(feature)[df.sort_values(feature)[classification] != label]
+            one = df.sort_values(feature)[df.sort_values(feature)[classification] == label]
+            fig.add_trace(go.Histogram(y=zero[groupby], x=zero[feature],
+                histfunc=agg, 
                 marker_color='blue',
-                opacity=0.5,
-                xbins=bins), 
-                row=i//2+1, col=i % 2 + 1
+                orientation='h',
+                opacity=0.5), 
+                row=i//n_columns+1, col=i % n_columns + 1
                 )
-            fig.add_trace(go.Histogram(x=one[feature],
+            fig.add_trace(go.Histogram(y=one[groupby], x=one[feature],
+                histfunc=agg, 
                 marker_color='red',
-                opacity=0.5,
-                xbins=bins),
-                row=i//2+1, col=i % 2 + 1)
+                orientation='h',
+                opacity=0.5),
+                row=i//n_columns+1, col=i % n_columns + 1)
         else:
-            fig.add_trace(go.Histogram(x=df[feature]), 
-            row=i//2+1, col=i % 2 + 1)
-    if color:
-        title = f'Observations with {color} of value {label} are indicated in red'
+            fig.add_trace(go.Histogram(
+                histfunc=agg, x=df[feature], y=df[groupby],
+                orientation='h'
+                ), 
+            row=i//n_columns+1, col=i % n_columns + 1)
+    
+    if classification:
+        title = f'Observations with {classification} of value {label} are indicated in red'
     else:
         title = 'Value counts'
-    fig.update_layout(height=300*round((len(columns)+.5)/2), 
-        showlegend=False,barmode=barmode,
-        bargap=0.1,
-        title = title,
+    fig.update_layout(
+        showlegend=False,
+        height=(n_rows+1)*height,
+        barmode=barmode,
+        # bargap=0.1,
+        title=title,
         title_x=0.5,
         title_xanchor='center',
-        title_y = 0,
-        title_yanchor = 'bottom')
+        # title_y=0.1,
+        title_yanchor='top'
+    )
+    fig.update_xaxes(title=dict(
+        standoff=0,
+        ),
+        title_text=f'{agg}'.upper(),
+        row=n_rows
+    )
+    if y_order:
+        print(f'y_order: {y_order}')
+        fig.update_yaxes(categoryorder='array', categoryarray=y_order)
     fig.show()
-    return fig
 
 # Function to plot multiple bar charts using Plotly. Show different colours based on classification.
 def plot_int_bar(df, columns=None, classification=None, label=1, barmode='stack', n_columns=1, height=150, y_order=None):
