1f62bb944ac646df42ea9c724180a746ea383915
silvhua <silvhua@gmail.com>
2024-01-01 18:31:36 -0800

    `plot_int_hist`: name the traces, show legend

--- silvhua_plot.py
+++ silvhua_plot.py
@@ -10,11 +10,10 @@ try:
 except:
     print('Matplotlib and Seaborn not installed.')
 
-# Function to plot multiple histograms using Plotly. Show different colours based on classification.
 def plot_int_hist(
     df, groupby, columns=None, classification=None, label=1, 
     agg='sum', title=True, y_order=False,
-    barmode='stack', n_columns=1, height=150
+    barmode='stack', n_columns=1, height=150, showlegend=False
     ):
     """
     Use Plotly to plot multiple histograms using the specified columns of a dataframe.
@@ -32,6 +31,7 @@ def plot_int_hist(
     - n_columns (optional): Number of columns in the figure. Default is 2.
     - height (optional): Height of each subplot in pixels. Default is 150.
     - y_order (optional): List of values to specify the order of the y axis. Default is None.
+    - showlegend (bool; optional): Whether to show the legend. Default is False.
 
     """
     if columns == None:
@@ -46,14 +46,18 @@ def plot_int_hist(
                 histfunc=agg, 
                 marker_color='blue',
                 orientation='h',
-                opacity=0.5), 
+                opacity=0.5,
+                name=f'{classification} not {label}'
+                ), 
                 row=i//n_columns+1, col=i % n_columns + 1
                 )
             fig.add_trace(go.Histogram(y=one[groupby], x=one[feature],
                 histfunc=agg, 
                 marker_color='red',
                 orientation='h',
-                opacity=0.5),
+                opacity=0.5,
+                name=f'{classification} is {label}'
+                ),
                 row=i//n_columns+1, col=i % n_columns + 1)
         else:
             fig.add_trace(go.Histogram(
@@ -69,7 +73,7 @@ def plot_int_hist(
     else:
         title = 'Value counts'
     fig.update_layout(
-        showlegend=False,
+        showlegend=showlegend,
         height=(n_rows+1)*height if height else None,
         barmode=barmode,
         # bargap=0.1,
@@ -95,70 +99,6 @@ def plot_int_hist(
         fig.update_yaxes(categoryorder='array', categoryarray=index_list)
     fig.show()
     return fig
-
-# Function to plot multiple bar charts using Plotly. Show different colours based on classification.
-def plot_int_bar(df, columns=None, classification=None, label=1, barmode='stack', n_columns=1, height=150, y_order=None):
-    """
-    Use Plotly to plot multiple histograms using the specified columns of a dataframe.
-    Arguments:
-    - df: Dataframe.
-    - columns (optional): Columns of dataframe on which to create the histogram. If blank, all numeric data will be plotted.
-    - classification (optional): Provide name of colum containing binary classification values 0 and 1. 
-        Data points classified as 1 will be in red.
-    - label (optional): Label of classification column. Default is 1.
-    - barmode ('stack', 'group', or 'overlay'; optional): How the different will be shown. Default is 'stack'.
-    - n_columns (optional): Number of columns in the figure. Default is 2.
-    - height (optional): Height of each subplot in pixels. Default is 150.
-    - y_order (optional): List of values to specify the order of the y axis. Default is None.
-
-    """
-    if columns == None:
-        columns = df.dtypes[df.dtypes != 'object'].index.tolist()
-    n_rows = (len(columns)-1) // n_columns + 1
-    fig = make_subplots(rows=n_rows, cols=n_columns, subplot_titles=columns)
-    for i, feature in enumerate(columns):
-        if classification:
-            zero = df.sort_values(feature)[df.sort_values(feature)[classification] != label]
-            one = df.sort_values(feature)[df.sort_values(feature)[classification] == label]
-            fig.add_trace(go.Histogram(y=zero[feature],
-                marker_color='blue',
-                orientation='h',
-                opacity=0.5), 
-                row=i//n_columns+1, col=i % n_columns + 1
-                )
-            fig.add_trace(go.Histogram(y=one[feature],
-                marker_color='red',
-                orientation='h',
-                opacity=0.5),
-                row=i//n_columns+1, col=i % n_columns + 1)
-        else:
-            fig.add_trace(go.Histogram(x=df[feature]), 
-            row=i//n_columns+1, col=i % n_columns + 1)
-    
-    if classification:
-        title = f'Observations with {classification} of value {label} are indicated in red'
-    else:
-        title = 'Value counts'
-    fig.update_layout(
-        showlegend=False,
-        height=(n_rows+1)*height,
-        barmode=barmode,
-        # bargap=0.1,
-        title=title,
-        title_x=0.5,
-        title_xanchor='center',
-        # title_y=0.1,
-        title_yanchor='top'
-    )
-    fig.update_xaxes(title=dict(
-        standoff=0,
-        ),
-        title_text='number of observations',
-        row=n_rows
-    )
-    if y_order:
-        fig.update_yaxes(categoryorder='array', categoryarray=y_order)
-    fig.show()
 
 # Function to plot multiple histograms
 def plot_hist(df, columns=None):
@@ -463,3 +403,67 @@ def correlation(df):
     # Create the heatmap with the mask and with annotation
     sns.heatmap(data=df.corr(numeric_only=True),mask=mask,annot=True)
     return df.corr()
+
+def plot_int_bar(df, columns=None, classification=None, label=1, barmode='stack', n_columns=1, height=150, y_order=None):
+    """
+    [DEPRECATED: use `plot_in_hist` instead]
+    Use Plotly to plot multiple histograms using the specified columns of a dataframe.
+    Arguments:
+    - df: Dataframe.
+    - columns (optional): Columns of dataframe on which to create the histogram. If blank, all numeric data will be plotted.
+    - classification (optional): Provide name of colum containing binary classification values 0 and 1. 
+        Data points classified as 1 will be in red.
+    - label (optional): Label of classification column. Default is 1.
+    - barmode ('stack', 'group', or 'overlay'; optional): How the different will be shown. Default is 'stack'.
+    - n_columns (optional): Number of columns in the figure. Default is 2.
+    - height (optional): Height of each subplot in pixels. Default is 150.
+    - y_order (optional): List of values to specify the order of the y axis. Default is None.
+
+    """
+    if columns == None:
+        columns = df.dtypes[df.dtypes != 'object'].index.tolist()
+    n_rows = (len(columns)-1) // n_columns + 1
+    fig = make_subplots(rows=n_rows, cols=n_columns, subplot_titles=columns)
+    for i, feature in enumerate(columns):
+        if classification:
+            zero = df.sort_values(feature)[df.sort_values(feature)[classification] != label]
+            one = df.sort_values(feature)[df.sort_values(feature)[classification] == label]
+            fig.add_trace(go.Histogram(y=zero[feature],
+                marker_color='blue',
+                orientation='h',
+                opacity=0.5), 
+                row=i//n_columns+1, col=i % n_columns + 1
+                )
+            fig.add_trace(go.Histogram(y=one[feature],
+                marker_color='red',
+                orientation='h',
+                opacity=0.5),
+                row=i//n_columns+1, col=i % n_columns + 1)
+        else:
+            fig.add_trace(go.Histogram(x=df[feature]), 
+            row=i//n_columns+1, col=i % n_columns + 1)
+    
+    if classification:
+        title = f'Observations with {classification} of value {label} are indicated in red'
+    else:
+        title = 'Value counts'
+    fig.update_layout(
+        showlegend=False,
+        height=(n_rows+1)*height,
+        barmode=barmode,
+        # bargap=0.1,
+        title=title,
+        title_x=0.5,
+        title_xanchor='center',
+        # title_y=0.1,
+        title_yanchor='top'
+    )
+    fig.update_xaxes(title=dict(
+        standoff=0,
+        ),
+        title_text='number of observations',
+        row=n_rows
+    )
+    if y_order:
+        fig.update_yaxes(categoryorder='array', categoryarray=y_order)
+    fig.show()
