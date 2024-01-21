42c0fcb46a6e0e7cca54adf070c5fb261425f72f
silvhua <silvhua@gmail.com>
2024-01-21 09:20:50 -0800

    created new conda env

---
+++ files/conda_env_personal.bat
@@ -0,0 +1,9 @@
+conda create -n personal && ^
+conda activate personal && ^
+conda install pip && ^
+conda install -y pandas && ^
+conda install -y requests && ^
+conda install -y ipykernel && ^
+conda install -y plotly && ^
+conda install -c conda-forge scrapy && ^
+pip install solara
