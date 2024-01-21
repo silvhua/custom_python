dd02c4c9f9a109feded44bd05dc03f32f1f678bd
silvhua <silvhua@gmail.com>
2024-01-21 09:20:26 -0800

    add `-y`; commented out some lines

--- files/conda_env.bat
+++ files/conda_env.bat
@@ -1,10 +1,13 @@
 conda create -n env_name && ^
 conda activate env_name && ^
+@REM conda install -y pip && ^
+@REM conda install -y openai && ^
+@REM conda install -y sqlalchemy && ^
+@REM pip install -y psycopg2 && ^
+@REM conda install -c conda-forge -y sshtunnel && ^
 conda install -y pandas && ^
-conda install -y openai && ^
-conda install -y sqlalchemy && ^
-pip install psycopg2 && ^
-conda install -c conda-forge -y sshtunnel && ^
 conda install -y requests && ^
 conda install -y ipykernel && ^
-conda install -y seaborn
+conda install -y plotly && ^
+conda install -y -c conda-forge scrapy && ^
+pip install -y solara
