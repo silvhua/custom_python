b2a6cc1f8b0c0e9361870b19d1c89224d1bfd26c
silvhua <silvhua@gmail.com>
2024-03-11 07:37:24 -0700

    return_duplicate_rows: print IDs of duplicate rows

--- wrangling.py
+++ wrangling.py
@@ -509,7 +509,7 @@ def filter_any_and_all_chain(
 
     return filtered_df
 
-def return_duplicate_rows(df, subset=None, keep=False):
+def return_duplicate_rows(df, subset=None, keep=False, id_column=None):
     """
     Identify duplicate rows in a dataframe.
 
@@ -526,11 +526,12 @@ def return_duplicate_rows(df, subset=None, keep=False):
     """
     print(f'DataFrame shape: {df.shape}')
     print(f'Number of duplicate rows: {df.duplicated(subset=subset, keep="first").sum()}')
+    if id_column:
+        print(f'{id_column} values of duplicate rows: {sorted(list(set(df[id_column])))}')
     duplicate_index = df.duplicated(subset=subset, keep=keep)
     duplicate_rows = df.loc[duplicate_index].sort_values(by=subset if subset else df.columns[0])
-    print(f'Returning {keep if keep else "all"} duplicate rows.')
+    print(f'\tReturning {keep if keep else "all"} duplicate rows.')
     return duplicate_rows
-
 
 def remove_duplicates_by_lettercase(df, column='Reference'):
     """
