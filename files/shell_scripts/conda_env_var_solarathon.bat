ca5fdb4f8314b8c8e27de5e6318bb377732490a5
silvhua <silvhua@gmail.com>
2024-03-28 01:20:28 -0700

    organize files

---
+++ files/shell_scripts/conda_env_var_solarathon.bat
@@ -0,0 +1,2 @@
+conda env config vars set ploomber_api_key=_Zdu8VBbr2A26jF86u3Y_ca_2MuAPPyY1NB2-rXkvdU && ^
+conda env config vars list
---
+++ files/shell_scripts/conda_env_var_stabilityai_credentials.bat
@@ -0,0 +1,2 @@
+conda env config vars set stability_api_key=sk-XUYB6MP04GB0wYmZ6NjExC3XEyDZvQ94qDDCLmvWyNDDkh9I && ^
+conda env config vars list
