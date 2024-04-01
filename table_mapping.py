2e58897ba012cd5873332e4390a539c8ab22d28d
silvhua <silvhua@gmail.com>
2024-04-01 16:11:35 -0700

    `merge_and_validate`: added param `fill_common_columns=False`

--- table_mapping.py
+++ table_mapping.py
@@ -174,6 +174,7 @@ def concat_columns(df, columns, new_column, sep='; ', drop_columns=False,
 def merge_and_validate(
         left_df, right_df, left_on, right_on, how='outer', indicator=True, drop_duplicates=False,
         nan_fill=None, left_df_name='left', right_df_name='right', drop_indictor_column=False, id_column='RID',
+        fill_common_columns=False,
         logger=None
         ):
     merged_df = pd.DataFrame()
@@ -220,11 +221,11 @@ def merge_and_validate(
             merge_info_message += f'\tShape after dropping duplicates: {merged_df.shape}\n'
         else:
             merge_info_message += f'\tDrop duplicates = {str(drop_duplicates)}\n'
-        
-        for column in common_columns:
-            if column != id_column:
-                debug_messages.append(f'Performing fillna on `{column}` column.')
-                merged_df[column] = merged_df[column].fillna(merged_df[f'{column}_y'])
+        if fill_common_columns == True:
+            for column in common_columns:
+                if column != id_column:
+                    debug_messages.append(f'Performing fillna on `{column}` column.')
+                    merged_df[column] = merged_df[column].fillna(merged_df[f'{column}_y'])
         if (right_on != left_on) & (right_on in common_columns):
             debug_messages.append(f'Performing fillna on `{left_on}` with `{right_on}_y` column.')
             merged_df[left_on] = merged_df[left_on].fillna(merged_df[f'{right_on}_y'])
