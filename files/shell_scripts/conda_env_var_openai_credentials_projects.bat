50c5d0b413ab33349a7b5c904b874ff814243832
silvhua <silvhua@gmail.com>
2024-04-16 16:15:17 -0700

    ignore

--- files/shell_scripts/conda_env_var_openai_credentials_projects.bat
+++
@@ -1,3 +0,0 @@
-conda env config vars set openai_api_key=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 && ^
-conda env config vars set openai_organization=org-abUfYkwR7vFsTFp1p0qZ6YtJ && ^
-conda env config vars list
