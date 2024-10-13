09896fbb9ccd5cf56714dca1b6ab093a0709c5bc
silvhua <silvhua@gmail.com>
2024-10-13 14:36:31 -0700

    updates to account for new computer

--- files/shell_scripts/conda_env_ginkgo.bat
+++ files/shell_scripts/conda_env_ginkgo.bat
@@ -1,10 +1,9 @@
-@REM conda create -n ginkgo && ^
-@REM conda activate ginkgo && ^
+conda create -n ginkgo && ^
+conda activate ginkgo && ^
 @REM conda install -y pip && ^
-@REM conda install -y pandas && ^
+conda install -y pandas && ^
 @REM conda install conda-forge::gspread  && ^
 @REM conda install conda-forge::oauth2client && ^
 @REM pip install psycopg2-binary && ^
-conda install conda-forge::python-dotenv
-
-@REM conda install -y ipykernel
+conda install conda-forge::python-dotenv && ^
+conda install -y ipykernel
